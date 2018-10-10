185c0cc9ac08b6dfaee82f93292a62e4e91143ef
TOURE NATHAN <toure21@live.com>
2018-10-10 03:18:51 +0300

    Added content to the Recipe Ingredient description activity

--- app/src/main/java/com/tourenathan/bakingapp/bakingapp/RecipeIngredientDescriptionActivityFragment.java
+++ app/src/main/java/com/tourenathan/bakingapp/bakingapp/RecipeIngredientDescriptionActivityFragment.java
@@ -1,22 +1,47 @@
 package com.tourenathan.bakingapp.bakingapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.gson.Gson;
+import com.tourenathan.bakingapp.bakingapp.adapter.RecipientIngredientDescriptionAdapter;
+import com.tourenathan.bakingapp.bakingapp.model.Recipe;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class RecipeIngredientDescriptionActivityFragment extends Fragment {
 
+    final static String TAG = RecipeIngredientDescriptionActivityFragment.class.getSimpleName();
+
+    RecipientIngredientDescriptionAdapter mRecipientIngredientDescriptionAdapter;
+    RecyclerView mRecipeDescriptionRecyclerview;
+
     public RecipeIngredientDescriptionActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recipe_ingredient_description, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_recipe_ingredient_description, container, false);
+        if (getActivity().getIntent()!=null && getActivity().getIntent().hasExtra(Intent.EXTRA_TEXT)) {
+            Log.d(TAG,getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT));
+            Gson gson = new Gson();
+            Recipe recipe = gson.fromJson(getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT), Recipe.class);
+            mRecipientIngredientDescriptionAdapter = new RecipientIngredientDescriptionAdapter();
+            mRecipeDescriptionRecyclerview = rootView.findViewById(R.id.recipe_description_ingredient_recyclerview);
+            mRecipientIngredientDescriptionAdapter.setData(recipe);
+            mRecipeDescriptionRecyclerview.setAdapter(mRecipientIngredientDescriptionAdapter);
+        } else {
+            getActivity().finish();
+        }
+        return rootView;
     }
 }
--- app/src/main/java/com/tourenathan/bakingapp/bakingapp/adapter/RecipientIngredientDescriptionAdapter.java
+++ app/src/main/java/com/tourenathan/bakingapp/bakingapp/adapter/RecipientIngredientDescriptionAdapter.java
@@ -2,31 +2,78 @@ package com.tourenathan.bakingapp.bakingapp.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.tourenathan.bakingapp.bakingapp.R;
+import com.tourenathan.bakingapp.bakingapp.model.Ingredient;
+import com.tourenathan.bakingapp.bakingapp.model.Recipe;
+import com.tourenathan.bakingapp.bakingapp.model.Step;
+
+import java.util.List;
 
 public class RecipientIngredientDescriptionAdapter extends RecyclerView.Adapter<RecipientIngredientDescriptionAdapter.ViewHolder> {
+
+    private final int RECIPE_INGREDIENT_TYPE = 1;
+   private final int RECIPE_DESCRIPTION_TYPE = 2;
+
+    private Recipe recipe;
+    private List<Step> recipeStepList;
+    private List<Ingredient> recipeIngredientList;
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(R.layout.recipe_description_list_item, parent, shouldAttachToParentImmediately);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0 && (recipeIngredientList.size() > 0)) ? RECIPE_INGREDIENT_TYPE : RECIPE_DESCRIPTION_TYPE;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if (getItemViewType(position) == RECIPE_INGREDIENT_TYPE) {
+            holder.recipeDescription.setText(R.string.recipe_ingredients);
+        } else {
+            if (recipeIngredientList.size() == 0) {
+                holder.recipeDescription.setText(recipe.getSteps().get(position).getShortDescription());
+            } else {
+                holder.recipeDescription.setText(recipe.getSteps().get(position - 1).getShortDescription());
+            }
 
+        }
     }
 
     @Override
     public int getItemCount() {
+        if (recipe != null) {
+            return (recipeIngredientList.size() > 0) ? recipeStepList.size() + 1 : recipeStepList.size();
+        }
         return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * Set the recipe which data will be displayed
+     *
+     * @param recipe
+     */
+    public void setData(Recipe recipe) {
+        this.recipe = recipe;
+        recipeIngredientList = recipe.getIngredients();
+        recipeStepList = recipe.getSteps();
+        notifyDataSetChanged();
+
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView recipeDescription;
 
